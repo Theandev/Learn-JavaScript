e0c893638adec4fe59a6b55f1b3db4d3622a0385
Theandev <chigthean@gmail.com>
2026-04-27 20:33:35 +0700

    Odd & Even

---
+++ OddAndEven.js
@@ -0,0 +1,10 @@
+var i = 0;
+for(let i = 1; i <= 100; i++) {
+    if(i % 2 === 0) {
+        console.log(i + " is even");
+    }    else {
+        console.log(i + " is odd");
+    }
+}
+
+console.log(i);
